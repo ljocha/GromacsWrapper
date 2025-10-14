af65df739e91de70817c2222efb5743f43e32f7f
Ales Krenek <ljocha@ics.muni.cz>
2025-10-14 16:44:39 +0000

    more robust, more async k8s

--- gromacs/k8s.py
+++ gromacs/k8s.py
@@ -12,9 +12,15 @@ class MDrunnerK8s(MDrunner):
     mdrun = 'gmx mdrun'
     mpiexec = 'mpiexec'
 
-    def __init__(self,pvc=None,workdir=None,image='cerit.io/ljocha/gromacs:2024-3-plumed-2-10-afed-pytorch-model-cv-2',**kwargs):
+    def __init__(self,pvc=None,workdir=None,image='cerit.io/ljocha/gromacs:2024-3-plumed-2-10-afed-pytorch-model-cv-2',mpi=1,omp=1,gpus=0,gputype='mig-1g.10gb',mem=4,jobname=None,**kwargs):
         super().__init__(**kwargs)
         self.image = image
+        self.mpi = mpi
+        self.omp = omp
+        self.gpus = gpus
+        self.gputype = gputype
+        self.mem = mem
+
         mnt = ''
 
 # heuristics to find PVC and working dir; can be overriden 
@@ -33,7 +39,21 @@ class MDrunnerK8s(MDrunner):
         self.workdir = workdir
         self.pvc = pvc
         self.mnt = mnt
-        self.jobname = "gmx-" + str(uuid.uuid4())
+        if jobname is None:
+            self.jobname = "gmx-" + str(uuid.uuid4())
+        else:
+            self.jobname = jobname
+
+    # start K8s job
+    def prehook(self,retry=1):
+#        if cores is not None and cores != mpi * omp:
+#            raise ValueError(f'cores ({cores}) != mpi ({mpi}) * omp ({omp})')
+#        if cores is None:
+#            cores = mpi * omp
+        cores = self.mpi * self.omp
+
+        with open(f'{self.mnt}/{self.workdir}/{self.jobname}.out','w') as out:
+            out.write('Log created\n')
 
         with open(f'{self.mnt}/{self.workdir}/{self.jobname}.sh','w') as script:
             script.write(f'''#!/bin/bash
@@ -42,17 +62,11 @@ while [ ! -f {self.jobname}.cmd ]; do
     sleep 1
 done
 
-exec >{self.jobname}.out 2>{self.jobname}.out
+exec >>{self.jobname}.out 2>>{self.jobname}.out
+echo Starting {self.jobname} payload >&2
 exec $(cat {self.jobname}.cmd)
 ''')
         os.chmod(f'{self.mnt}/{self.workdir}/{self.jobname}.sh',0o755)
-
-    # start K8s job
-    def prehook(self,cores=None,mpi=1,omp=1,gpus=0,gputype='mig-1g.10gb',mem=4,retry=1):
-        if cores is not None and cores != mpi * omp:
-            raise ValueError(f'cores ({cores}) != mpi ({mpi}) * omp ({omp})')
-        if cores is None:
-            cores = mpi * omp
 
         job = f"""\
 apiVersion: batch/v1
@@ -87,16 +101,16 @@ spec:
 
         env:
         - name: 'OMP_NUM_THREADS'
-          value: '{omp}'
+          value: '{self.omp}'
         resources:
           requests:
             cpu: '{cores}'
-            memory: {mem}Gi
-            nvidia.com/{gputype}: {gpus}
+            memory: {self.mem}Gi
+            nvidia.com/{self.gputype}: {self.gpus}
           limits:
             cpu: '{cores}'
-            memory: {mem}Gi
-            nvidia.com/{gputype}: {gpus}
+            memory: {self.mem}Gi
+            nvidia.com/{self.gputype}: {self.gpus}
         volumeMounts:
         - name: vol-1
           mountPath: /mnt
@@ -109,17 +123,18 @@ spec:
             y.write(job)
             y.flush()
             os.system(f'kubectl apply -f {y.name}')
-            for _ in range(retry):
-                os.system(f'kubectl wait --for=condition=ready pod -l job={self.jobname}')
+#            for _ in range(retry):
+#                s=os.system(f'kubectl wait --for=condition=ready pod -l job={self.jobname} --timeout=3600s')
+#                if os.WEXITSTATUS(s) == 0:
+#                    break
 
 
 
-    # cleanup the K8s job
-    def posthook(self):
-#        os.system(f'kubectl delete job/{self.jobname}')
-        os.remove(f'{self.mnt}/{self.workdir}/{self.jobname}.sh')
-        os.remove(f'{self.mnt}/{self.workdir}/{self.jobname}.cmd')
-#        os.remove(f'{os.getcwd()}/{self.workdir}/{self.jobname}.out')
+#    def posthook(self):
+##        os.system(f'kubectl delete job/{self.jobname}')
+#        os.remove(f'{self.mnt}/{self.workdir}/{self.jobname}.sh')
+#        os.remove(f'{self.mnt}/{self.workdir}/{self.jobname}.cmd')
+##        os.remove(f'{os.getcwd()}/{self.workdir}/{self.jobname}.out')
 
     def commandline(self, **kwargs):
 #        return ['kubectl','exec','-ti',f'job/{self.jobname}','--'] + super().commandline(**kwargs)
@@ -127,12 +142,20 @@ spec:
             cmd.write(" ".join(super().commandline(**kwargs)))
 
         os.rename(f'{self.mnt}/{self.workdir}/{self.jobname}.tmp',f'{self.mnt}/{self.workdir}/{self.jobname}.cmd')
-        return [ 'tail', '-f', f'{self.mnt}/{self.workdir}/{self.jobname}.out' ]
+#        return [ 'tail', '-f', f'{self.mnt}/{self.workdir}/{self.jobname}.out' ]
+        return ['kubectl','get',f'job/{self.jobname}']
+
+    def run(self,**kwargs):
+#    mdrunner.run(pre={'omp':ompthreads,'mpi':mpiranks,'gpus':gpus, 'retry':10 }, mdrunargs={**kwargs,'ntomp':ompthreads,'pin':'on'},ncores=mpiranks)
+        return super().run(mdrunargs = { **kwargs, 'ntomp': self.omp, 'pin':'on'}, ncores=self.mpi)
+
 
     def kill(self):
+        os.remove(f'{self.mnt}/{self.workdir}/{self.jobname}.sh')
+        os.remove(f'{self.mnt}/{self.workdir}/{self.jobname}.cmd')
         os.system(f'kubectl delete job/{self.jobname}')
         
-    def log(self):
+    def log(self,tail=0):
         with open(f'{self.mnt}/{self.workdir}/{self.jobname}.out') as log:
-            for l in log:
+            for l in list(log)[-tail:]:
                 print(l,end='')
